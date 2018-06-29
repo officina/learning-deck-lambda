a6c1540fb1672c9c7b2366174c51eef010b44058
Nicola Zambon <nicola.zambon@officina.cc>
2018-06-29 16:38:59 +0200

    refactoring host

--- handler_stub.py
+++ handler_stub.py
@@ -8,58 +8,27 @@ import simplejson as sjson
 
 
 def user_status_action(event, context):
-    result = GameController.user_status_action(event, context)
-    response = {
-        'statusCode': 200,
-        "body": json.dumps(result['body'])
-    }
-    return response
+	result = GameController.user_status_action(event, context)
+	response = {
+		'statusCode': result['statusCode'],
+		"body": json.dumps(result['body'])
+	}
+	return response
 
-def get_user_status(event, context):
-    print("*************************************")
-    print(event)
-    print("*************************************")
-    print(context)
-    print("*************************************")
-    body = {
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
 
 def play_action(event, context):
-    print("*************************************")
-    print(event)
-    print("*************************************")
-    print(context)
-    print("*************************************")
-    body = {
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+	result = GameController.play_action(event, context)
+	response = {
+		'statusCode': result['statusCode'],
+		"body": json.dumps(result['body'])
+	}
+	return response
 
 
-def user_upgrade(event, context):
-    print("*************************************")
-    print(event)
-    print("*************************************")
-    print(context)
-    print("*************************************")
-    body = {
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+def level_upgrade(event, context):
+	result = GameController.user_status_action(event, context)
+	response = {
+		'statusCode': result['statusCode'],
+		"body": json.dumps(result['body'])
+	}
+	return response
