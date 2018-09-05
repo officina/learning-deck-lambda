98a0c4e21d3b18c3db0e0e0c9658a58bbc4fda9e
Nicola Zambon <nicola.zambon@officina.cc>
2018-09-05 14:51:47 +0200

    accessi diversificati sui tre ambienti

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Il parametro "resultTtlInSeconds" consente di configurare la durata della cache (in secondi)
 # Impostandolo a 0 la cache viene disabilitata.
-sls deploy --stage prod --profile mygenerali-prod --endpointType private --resultTtlInSeconds 0
+sls deploy --stage prod --profile mygenerali-prod --resultTtlInSeconds 0
