6ee5059986acf1578b0f2434f8fc573f0aa84e22
Nicola Zambon <nicola.zambon.1985@gmail.com>
2019-01-15 12:12:14 +0100

    avanzamento

--- gamecontroller/handler.py
+++ gamecontroller/handler.py
@@ -57,18 +57,23 @@ def get_user_status(event, context, player, playoff_client):
     if event["queryStringParameters"] is not None and "state" in event["queryStringParameters"]:
         state_ = event["queryStringParameters"]["state"]
     try:
-        # calcolo prima il ranking per consentire se necessaro di "attivare" il boot di player appena creati
-        result_ranking = playoff_client.get(
-            route="/runtime/leaderboards/progressione_personale",
-            query={
-                "player_id": player,
-                "cycle": "alltime",
-                "entity_id": player,
-                "radius": "0",
-                "sort": "descending",
-                "ranking": "relative"
-            })
-        result = playoff_client.get(route=f"/admin/players/{player}")
+        if state_ == 'READY':
+            try:
+                UserReady.get(player).update_playoff_user_profile(playoff_client)
+                user_info = UserReady.get(player).update_playoff_user_ranking(playoff_client)
+            except UserReady.DoesNotExist:
+                UserReady(player).save()
+                UserReady(player).update_playoff_user_profile(playoff_client)
+                user_info = UserReady(player).update_playoff_user_ranking(playoff_client)
+        else:
+            try:
+                User.get(player).update_playoff_user_profile(playoff_client)
+                user_info = User.get(player).update_playoff_user_ranking(playoff_client)
+            except User.DoesNotExist:
+                User(player).save()
+                User(player).update_playoff_user_profile(playoff_client)
+                user_info = User(player).update_playoff_user_ranking(playoff_client)
+
     except PlayoffException as err:
         print(err)
         if err.name == 'player_not_found':
@@ -77,22 +82,16 @@ def get_user_status(event, context, player, playoff_client):
             return playoff_error_response(err.message)
 
     # get_weeks is the only action of Mapping that would require aws, so I leave it here
-    print("RANKING CALCULATION")
-    ranking = (result_ranking['data'][0]['rank'] / result_ranking['total'] * 100) / 100
-    print("WEEKS CALCULATION")
-    weeks = get_weeks(player, state_)
-    if state_ == 'READY':
-        UserReady.get(player).save_playoff_user_profile(playoff_client)
-        UserReady.get(player).save_playoff_user_ranking(playoff_client)
-        date_last_play = UserReady.get(player).date_last_play_timestamp_format
-        print("READY:")
-        print(date_last_play)
-    else:
-        print("PUBLISHED:")
-        date_last_play = User.get(player).date_last_play_timestamp_format
-        print(date_last_play)
+    print(user_info)
+    ranking_info = user_info.playoff_user_ranking_dict_format
+    ranking = (ranking_info['data'][0]['rank'] / ranking_info['total'] * 100) / 100
+
+    weeks_info = user_info.unblocked_weeks
+    user_profile_info = user_info.playoff_user_profile
+    date_last_play = user_info.date_last_play_timestamp_format
+
     print("MAPPING START")
-    return Mapping(result, weeks, ranking=ranking, date_last_play=date_last_play).json
+    return Mapping(user_profile_info, weeks_info, ranking=ranking, date_last_play=date_last_play).json
 
 
 def get_weeks(player, state="PUBLISHED"):
