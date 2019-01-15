a33691eb1bfd080fbff42c786291d8d2c2c9b8c3
Nicola Zambon <nicola.zambon.1985@gmail.com>
2019-01-15 14:57:09 +0100

    irrobustimento scripts di deploy

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Il parametro "resultTtlInSeconds" consente di configurare la durata della cache (in secondi)
 # Impostandolo a 0 la cache viene disabilitata.
+export AWS_PROFILE=mygenerali-dev
 node_modules/serverless/bin/serverless deploy --profile mygenerali-dev --resultTtlInSeconds 0 -v
