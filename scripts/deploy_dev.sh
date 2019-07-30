d88c3906a75019698cfd5d96ac8844b5dc17d16e
Nicola Zambon <nicola.zambon.1985@gmail.com>
2019-07-30 08:50:09 +0200

    details

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -2,4 +2,4 @@
 # Il parametro "resultTtlInSeconds" consente di configurare la durata della cache (in secondi)
 # Impostandolo a 0 la cache viene disabilitata.
 #export AWS_PROFILE=default
-node_modules/serverless/bin/serverless deploy --profile learning-deck-dev --resultTtlInSeconds 0 -v
+node_modules/serverless/bin/serverless deploy --profile leroy-merlin-dev --resultTtlInSeconds 0 -v
